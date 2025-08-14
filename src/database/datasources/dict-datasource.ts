3c352d61cc0bcfbf99bb3635d925ff3ffd9606a4
igoralexandrescruz-create <igor.alexandre.s.cruz@gmail.com>
2025-08-14 11:07:14 -0300

    feat: datasource

---
+++ src/database/datasources/dict-datasource.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import { env } from '../../config/env';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+export const DictDatasourceModule = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: env.database.host,
+  port: env.database.port,
+  username: env.database.username,
+  password: env.database.password,
+  database: env.database.database,
+  entities: [`${__dirname}/../entities/*/*.{ts,js}`],
+  synchronize: false,
+  logging: env.server.debug,
+});
+
+//Para migrations apenas
+export const DictDatasourceMigration = new DataSource({
+  type: 'postgres',
+  host: env.database.host,
+  port: env.database.port,
+  username: env.database.username,
+  password: env.database.password,
+  database: env.database.database,
+  migrations: [`${__dirname}/../migrations/*.ts`],
+  subscribers: [],
+  synchronize: false,
+  logging: true,
+});
