3fbbe9e71d3078ed4ca5ac8109303b820b0bf4da
igoralexandrescruz-create <igor.alexandre.s.cruz@gmail.com>
2025-08-14 11:07:01 -0300

    feat: jwt and database env

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,4 +11,21 @@ export const env = {
             protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http',
         },
     },
+    security: {
+        jwt: {
+            jwtSecret: process.env.JWT_SECRET,
+            jwtExpiresIn: +process.env.JWT_EXPIRES_IN || 600,
+            jwtExpiresInApp: +process.env.JWT_EXPIRES_IN_APP || 43200,
+        },
+        hash: {
+            rounds: parseInt(process.env.HASH_ROUNDS, 10) || 12,
+        }
+    },
+    database: {
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+        username: process.env.DATABASE_USERNAME || 'postgres',
+        password: process.env.DATABASE_PASSWORD || 'postgres',
+        database: process.env.DATABASE_NAME || 'nexa',
+    },
 };
