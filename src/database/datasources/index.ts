1fdd041e733fc7730556eb4168c71324b52c24dc
igoralexandrescruz-create <igor.alexandre.s.cruz@gmail.com>
2025-08-15 14:10:10 -0300

    change: datasource location

--- src/database/datasources/index.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DictDatasourceModule } from './dict-datasource';
-
-@Module({
-  imports: [DictDatasourceModule],
-  controllers: [],
-  providers: [],
-})
-export class DictDatasourcesModule { }
