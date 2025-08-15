74e0ff1ef39b3c1a63785d940140ea70baf1e876
igoralexandrescruz-create <igor.alexandre.s.cruz@gmail.com>
2025-08-15 14:08:48 -0300

    feat: redis env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ export const env = {
     server: {
         port: +process.env.SERVER_PORT || 3000,
         reverseProxy: process.env.REVERSE_PROXY === 'true',
-        prefix: process.env.SERVER_PREFIX || 'api/v1',
+        prefix: process.env.SERVER_PREFIX || 'api',
         url: process.env.SERVER_URL || 'localhost',
         debug: process.env.DEBUG === 'true',
         info: {
@@ -15,7 +15,6 @@ export const env = {
         jwt: {
             jwtSecret: process.env.JWT_SECRET,
             jwtExpiresIn: +process.env.JWT_EXPIRES_IN || 600,
-            jwtExpiresInApp: +process.env.JWT_EXPIRES_IN_APP || 43200,
         },
         hash: {
             rounds: parseInt(process.env.HASH_ROUNDS, 10) || 12,
@@ -28,4 +27,11 @@ export const env = {
         password: process.env.DATABASE_PASSWORD || 'postgres',
         database: process.env.DATABASE_NAME || 'nexa',
     },
+    redis: {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+        username: process.env.REDIS_USERNAME || '',
+        password: process.env.REDIS_PASSWORD || '',
+        db: parseInt(process.env.REDIS_DB, 10) || 0,
+    }
 };
