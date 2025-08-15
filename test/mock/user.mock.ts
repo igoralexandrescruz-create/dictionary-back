030c82fa0b37c7da9fec0944aaa82b3c6765e7a7
igoralexandrescruz-create <igor.alexandre.s.cruz@gmail.com>
2025-08-15 17:16:48 -0300

    change: user params

--- test/mock/user.mock.ts
+++ test/mock/user.mock.ts
@@ -2,12 +2,26 @@ import { IdPublic } from "src/modules/_shared/domain/value-objects/id-public";
 import { User } from "src/modules/users/domain/entities";
 import { Email } from "src/modules/users/domain/value-objects/email";
 
-export const mockUser = (id = 1): User => {
+export const mockUser = ({
+    id = 1,
+    idPublic = '01K2QK7J9S8BAPZTA17V7C5427',
+    email = 'test@test.com',
+    password = '123456',
+    name = 'Test User'
+}: UserMock): User => {
     return User.create({
         id,
-        idPublic: IdPublic.create('01K2QK7J9S8BAPZTA17V7C5427').idPublic,
-        email: Email.create('test@test.com').email,
-        password: '123456',
-        name: 'Test User',
+        idPublic: IdPublic.create(idPublic).idPublic,
+        email: Email.create(email).email,
+        password,
+        name,
     })
+}
+
+type UserMock = {
+    id?: number;
+    idPublic?: string;
+    email?: string;
+    password?: string;
+    name?: string;
 }
